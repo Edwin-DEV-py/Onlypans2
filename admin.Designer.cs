63d1b7977dcdea7325f22bbbbf7df3c54a6eaa09
edwin-tarazona <nicolas.tarazona.morales@gmail.com>
2021-09-07 07:12:18 -0500

    cambios5

--- admin.Designer.cs
+++ admin.Designer.cs
@@ -44,6 +44,7 @@ namespace Onlypans
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.BurlyWood;
             this.ClientSize = new System.Drawing.Size(739, 311);
             this.Controls.Add(this.cajarica2);
             this.Name = "admin";
